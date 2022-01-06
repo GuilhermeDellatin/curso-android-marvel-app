52877fb904592264b817ae6b870fd84fc72f4a4f
GuilhermeDellatin <guidellatin@gmail.com>
2022-01-05 22:22:56 -0300

    Creating nav graph

--- app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.gfdellatin.curso_android_marvel_app.R
 import com.gfdellatin.curso_android_marvel_app.databinding.ActivityMainBinding
 
@@ -22,6 +23,9 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_container) as NavHostFragment
 
         navController = navHostFragment.navController
+
+        binding.bottomNavMain.setupWithNavController(navController)
+
         appBarConfiguration = AppBarConfiguration(
             setOf()
         )
