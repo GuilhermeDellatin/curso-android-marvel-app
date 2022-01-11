5ac0aedd5b506d0e030a86711cc9f047d58f2706
GuilhermeDellatin <guidellatin@gmail.com>
2022-01-10 22:13:02 -0300

    Mapping character endpoint

---
+++ core/src/main/java/com/gfdellatin/core/data/network/MarvelApi.kt
@@ -0,0 +1,15 @@
+package com.gfdellatin.core.data.network
+
+import com.gfdellatin.core.data.network.response.DataWrapperResponse
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface MarvelApi {
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @QueryMap
+        queries: Map<String, String>
+    ): DataWrapperResponse
+
+}
