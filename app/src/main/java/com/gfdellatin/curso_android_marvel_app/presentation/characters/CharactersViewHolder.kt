ba521011714651e79b62a59d071cffd2a76e792d
GuilhermeDellatin <guidellatin@gmail.com>
2022-01-17 22:47:12 -0300

    Create ViewHolder and Character Data class

---
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersViewHolder.kt
@@ -0,0 +1,34 @@
+package com.gfdellatin.curso_android_marvel_app.presentation.characters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.gfdellatin.core.domain.model.Character
+import com.gfdellatin.curso_android_marvel_app.R
+import com.gfdellatin.curso_android_marvel_app.databinding.ItemCharacterBinding
+
+class CharactersViewHolder(
+    itemCharacterBinding: ItemCharacterBinding
+) : RecyclerView.ViewHolder(itemCharacterBinding.root) {
+
+    private val textName = itemCharacterBinding.textName
+    private val imageCharacter = itemCharacterBinding.imageCharacter
+
+    fun bind(character: Character) {
+        textName.text = character.name
+        Glide.with(itemView)
+            .load(character.imageUrl)
+            .fallback(R.drawable.ic_img_loading_error)
+            .into(imageCharacter)
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): CharactersViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            val itemBinding = ItemCharacterBinding.inflate(inflater, parent, false)
+            return CharactersViewHolder(itemBinding)
+        }
+    }
+
+}
---
+++ core/src/main/java/com/gfdellatin/core/domain/model/Character.kt
@@ -0,0 +1,6 @@
+package com.gfdellatin.core.domain.model
+
+data class Character(
+    val name: String,
+    val imageUrl: String
+)
