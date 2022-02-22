2f03610a556838949b1e781818a151914904daa2
GuilhermeDellatin <guidellatin@gmail.com>
2022-02-21 22:30:21 -0300

    Create tearDownDispatcher to reset and clean dispatcher CharactersViewModelTest.kt

--- app/src/test/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersViewModelTest.kt
+++ app/src/test/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersViewModelTest.kt
@@ -12,8 +12,10 @@ import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
@@ -80,4 +82,12 @@ class CharactersViewModelTest {
 
         charactersViewModel.charactersPagingData("")
     }
+
+    @ExperimentalCoroutinesApi
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
 }
