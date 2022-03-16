b02cbd080ddda182254c694e7ac229ea404bc9a8
GuilhermeDellatin <guidellatin@gmail.com>
2022-03-15 21:33:33 -0300

    Configuring Hilt for ui testing

--- app/src/androidTest/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersFragmentTest.kt
@@ -2,12 +2,19 @@ package com.gfdellatin.curso_android_marvel_app.presentation.characters
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class CharactersFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
 
     @Before
     fun setUp() {
