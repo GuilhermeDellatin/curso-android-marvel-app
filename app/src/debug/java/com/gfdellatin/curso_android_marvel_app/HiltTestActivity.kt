1d5100f8b43b95333faa5b9837ec0362cf0d0369
GuilhermeDellatin <guidellatin@gmail.com>
2022-03-15 22:12:41 -0300

    Configuring Hilt for ui testing

---
+++ app/src/debug/java/com/gfdellatin/curso_android_marvel_app/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.gfdellatin.curso_android_marvel_app
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
