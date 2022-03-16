cb4ad73a8296bfc21f35fbb76220e35c31d3d37f
GuilhermeDellatin <guidellatin@gmail.com>
2022-03-15 22:13:16 -0300

    Adjust ui test in CharactersFragmentTest.kt

--- app/src/androidTest/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/gfdellatin/curso_android_marvel_app/presentation/characters/CharactersFragmentTest.kt
@@ -1,7 +1,12 @@
 package com.gfdellatin.curso_android_marvel_app.presentation.characters
 
-import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.gfdellatin.curso_android_marvel_app.R
+import com.gfdellatin.curso_android_marvel_app.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
@@ -18,11 +23,13 @@ class CharactersFragmentTest {
 
     @Before
     fun setUp() {
-        launchFragmentInContainer<CharactersFragment>()
+        launchFragmentInHiltContainer<CharactersFragment>()
     }
 
     @Test
     fun shouldShowCharacters_whenViewIsCreated() {
-
+        onView(
+            withId(R.id.recycler_characters)
+        ).check(matches(isDisplayed()))
     }
 }
