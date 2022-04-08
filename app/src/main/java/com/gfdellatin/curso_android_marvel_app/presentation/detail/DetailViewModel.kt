a9c3356eadfa9aecc4e7e2901006acff1a518e67
GuilhermeDellatin <guidellatin@gmail.com>
2022-04-07 23:06:30 -0300

    Implement DetailViewModel.kt

--- app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/detail/DetailViewModel.kt
@@ -1,7 +1,45 @@
 package com.gfdellatin.curso_android_marvel_app.presentation.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gfdellatin.core.domain.model.Comic
+import com.gfdellatin.core.usecase.GetComicsUseCase
+import com.gfdellatin.core.usecase.base.ResultStatus
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DetailViewModel : ViewModel() {
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val getComicsUseCase: GetComicsUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableLiveData<UiState>()
+    val uiState: LiveData<UiState> get() = _uiState
+
+    fun getComics(characterId: Int) = viewModelScope.launch {
+        getComicsUseCase(GetComicsUseCase.GetComicsParams(characterId))
+            .watchStatus()
+    }
+
+    private fun Flow<ResultStatus<List<Comic>>>.watchStatus() = viewModelScope.launch {
+        collect { status ->
+            when (status) {
+                ResultStatus.Loading -> UiState.Loading
+                is ResultStatus.Success -> UiState.Success(status.data)
+                is ResultStatus.Error -> UiState.Error
+            }
+        }
+    }
+
+    sealed class UiState {
+        object Loading: UiState()
+        data class Success(val comics: List<Comic>) : UiState()
+        object Error : UiState()
+    }
 
 }
