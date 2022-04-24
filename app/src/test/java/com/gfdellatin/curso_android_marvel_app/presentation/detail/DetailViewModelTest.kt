036337b744880ea059dd59d38c875737e1fb3f88
GuilhermeDellatin <guidellatin@gmail.com>
2022-04-24 13:55:02 -0300

    Create initial DetailViewModelTest.kt

---
+++ app/src/test/java/com/gfdellatin/curso_android_marvel_app/presentation/detail/DetailViewModelTest.kt
@@ -0,0 +1,44 @@
+package com.gfdellatin.curso_android_marvel_app.presentation.detail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.gfdellatin.core.usecase.GetCharacterCategoriesUseCase
+import com.gfdellatin.testing.MainCoroutineRule
+import com.gfdellatin.testing.model.CharacterFactory
+import com.gfdellatin.testing.model.ComicFactory
+import com.gfdellatin.testing.model.EventFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class DetailViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    private lateinit var getCharacterCategoriesUseCase: GetCharacterCategoriesUseCase
+
+    @Mock
+    private lateinit var uiStateObserver: Observer<DetailViewModel.UiState>
+
+    private val character = CharacterFactory().create(CharacterFactory.Hero.ThreeDMan)
+    private val comics = listOf(ComicFactory().create(ComicFactory.FakeComic.FakeComic1))
+    private val events = listOf(EventFactory().create(EventFactory.FakeEvent.FakeEvent1))
+
+    private lateinit var detailViewModel: DetailViewModel
+
+    @Before
+    fun setUp() {
+        detailViewModel = DetailViewModel(getCharacterCategoriesUseCase)
+        detailViewModel.uiState.observeForever(uiStateObserver)
+    }
+}
