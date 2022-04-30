4a8876fcbd430638d85ea2292afd8fbb878b878c
GuilhermeDellatin <guidellatin@gmail.com>
2022-04-30 15:01:46 -0300

    Create entity for save favorite in local data base using Room

---
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/framework/db/entity/FavoriteEntity.kt
@@ -0,0 +1,17 @@
+package com.gfdellatin.curso_android_marvel_app.framework.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.gfdellatin.core.data.DbConstants
+
+@Entity(tableName = DbConstants.FAVORITES_TABLE_NAME)
+data class FavoriteEntity(
+    @PrimaryKey
+    @ColumnInfo(name = DbConstants.FAVORITES_COLUMN_INFO_ID)
+    val id: Int,
+    @ColumnInfo(name = DbConstants.FAVORITES_COLUMN_INFO_NAME)
+    val name: String,
+    @ColumnInfo(name = DbConstants.FAVORITES_COLUMN_INFO_IMAGE_URL)
+    val imageUrl: String
+)
---
+++ core/src/main/java/com/gfdellatin/core/data/DbConstants.kt
@@ -0,0 +1,8 @@
+package com.gfdellatin.core.data
+
+object DbConstants {
+    const val FAVORITES_TABLE_NAME = "favorites"
+    const val FAVORITES_COLUMN_INFO_ID = "id"
+    const val FAVORITES_COLUMN_INFO_NAME = "name"
+    const val FAVORITES_COLUMN_INFO_IMAGE_URL = "image_url"
+}
