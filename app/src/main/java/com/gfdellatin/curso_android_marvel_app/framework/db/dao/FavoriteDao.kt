7372b281010ceac93e996d58a062e95345a8bcf9
GuilhermeDellatin <guidellatin@gmail.com>
2022-04-30 15:52:59 -0300

    Create DAO

---
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/framework/db/dao/FavoriteDao.kt
@@ -0,0 +1,19 @@
+package com.gfdellatin.curso_android_marvel_app.framework.db.dao
+
+import androidx.room.*
+import com.gfdellatin.core.data.DbConstants
+import com.gfdellatin.curso_android_marvel_app.framework.db.entity.FavoriteEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+
+    @Query("SELECT * FROM ${DbConstants.FAVORITES_TABLE_NAME}")
+    suspend fun loadFavorites(): Flow<List<FavoriteEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favoriteEntity: FavoriteEntity)
+
+    @Delete
+    suspend fun deleteFavorite(favoriteEntity: FavoriteEntity)
+}
