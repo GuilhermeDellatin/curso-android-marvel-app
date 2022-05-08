8cfbfaf613f5a2e3ce25cef75b0f90d52443f371
GuilhermeDellatin <guidellatin@gmail.com>
2022-05-08 16:04:48 -0300

    Add new action in FavoriteUiActionStateLiveData.kt

--- app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/detail/DetailViewModel.kt
@@ -2,6 +2,7 @@ package com.gfdellatin.curso_android_marvel_app.presentation.detail
 
 import androidx.lifecycle.ViewModel
 import com.gfdellatin.core.usecase.AddFavoriteUseCase
+import com.gfdellatin.core.usecase.CheckFavoriteUseCase
 import com.gfdellatin.core.usecase.GetCharacterCategoriesUseCase
 import com.gfdellatin.core.usecase.base.CoroutinesDispatchers
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,6 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class DetailViewModel @Inject constructor(
     getCharacterCategoriesUseCase: GetCharacterCategoriesUseCase,
+    checkFavoriteUseCase: CheckFavoriteUseCase,
     addFavoriteUseCase: AddFavoriteUseCase,
     coroutinesDispatchers: CoroutinesDispatchers
 ) : ViewModel() {
@@ -21,12 +23,8 @@ class DetailViewModel @Inject constructor(
 
     val favorite = FavoriteUiActionStateLiveData(
         coroutinesDispatchers.main(),
+        checkFavoriteUseCase,
         addFavoriteUseCase
     )
-
-
-    init {
-        favorite.setDefault()
-    }
 
 }
