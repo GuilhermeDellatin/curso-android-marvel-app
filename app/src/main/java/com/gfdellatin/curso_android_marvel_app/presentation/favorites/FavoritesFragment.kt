c3cc5bfd833e41d27945010ff378d595ea4031e8
GuilhermeDellatin <guidellatin@gmail.com>
2022-05-11 15:52:04 -0300

    Create FavoritesAdapter

--- app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/favorites/FavoritesFragment.kt
@@ -1,19 +1,30 @@
 package com.gfdellatin.curso_android_marvel_app.presentation.favorites
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gfdellatin.curso_android_marvel_app.R
+import androidx.fragment.app.Fragment
 import com.gfdellatin.curso_android_marvel_app.databinding.FragmentFavoritesBinding
+import com.gfdellatin.curso_android_marvel_app.framework.imageloader.ImageLoader
+import com.gfdellatin.curso_android_marvel_app.presentation.common.getGenericAdapterOf
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
     private val binding: FragmentFavoritesBinding get() = _binding!!
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    private val favoritesAdapter by lazy {
+        getGenericAdapterOf {
+            FavoritesViewHolder.create(it, imageLoader)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +36,18 @@ class FavoritesFragment : Fragment() {
     ).apply {
         _binding = this
     }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initFavoritesAdapter()
+    }
+
+    private fun initFavoritesAdapter() {
+        binding.recyclerFavorites.run {
+            setHasFixedSize(true)
+            adapter = favoritesAdapter
+        }
+    }
 
     override fun onDestroyView() {
         _binding = null
