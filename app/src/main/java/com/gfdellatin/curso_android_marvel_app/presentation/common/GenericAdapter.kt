787c514e8d1f6acc667671d9c50c41273e27bd0a
GuilhermeDellatin <guidellatin@gmail.com>
2022-05-11 10:38:23 -0300

    Create Generic function to the create list adapters

---
+++ app/src/main/java/com/gfdellatin/curso_android_marvel_app/presentation/common/GenericAdapter.kt
@@ -0,0 +1,23 @@
+package com.gfdellatin.curso_android_marvel_app.presentation.common
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+
+inline fun <T: ListItem, VH: GenericViewHolder<T>> getGenericAdapterOf(
+    crossinline createViewHolder: (ViewGroup) -> VH
+) : ListAdapter<T, VH> {
+
+    val diff = GenericDiffCallback<T>()
+
+    return object : ListAdapter<T, VH>(diff) {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+            return createViewHolder(parent)
+        }
+
+        override fun onBindViewHolder(holder: VH, position: Int) {
+            holder.bind(getItem(position))
+        }
+
+    }
+}
